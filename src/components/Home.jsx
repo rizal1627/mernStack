8ec1b8b36d527e3c4cb5f1e7ad8ac07b33dbd72e
rizal1627 <setioajirizal@gmail.com>
2023-03-06 22:29:45 +0700

    Update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
             <Navbar />
             <div className="container">
             <h1 className="w-40 my-5 mx-2 text-xl font-extrabold">Home Page Dashboard</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim repellendus tempore veniam eos dignissimos architecto iure facere ut id eaque in nam itaque laboriosam dolor repudiandae, saepe possimus unde totam?</p>
             <ProductData />
             </div>
         </div>
